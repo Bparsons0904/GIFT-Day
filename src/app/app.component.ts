dce422bc9b67e1a97592ebfbc091e8dc078726fd
Unknown <Deadstylebp@gmail.com>
2018-02-21 08:42:19 -0600

    Rebase from scractch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
-import { environment } from '../environments/environment';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  }
-
+  title = 'app';
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDMksH94ia1yDrXNcee-l244JaLOeYE3b4",
+    authDomain: "gift-day.firebaseapp.com",
+    databaseURL: "https://gift-day.firebaseio.com",
+    projectId: "gift-day",
+    storageBucket: "gift-day.appspot.com",
+    messagingSenderId: "855560836446"
+  }
 };
